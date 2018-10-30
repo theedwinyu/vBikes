f4806849c47b2706dedd657fd76ecd1c6dfd0f22
theedwinyu <theedwinyu888@gmail.com>
2018-10-30 01:11:40 -0400

    got scrolling animation

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1324,3 +1324,11 @@ $(document).ready(function(){
         removeLine(theValue);
     });
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
